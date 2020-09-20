f2b4a32a3cf5c9b74f07aeb2fdf3286b3a45aaf7
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-09-20 12:30:42 -0300

    EducAll - Front-end

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,23 +1,34 @@
-import React, { InputHTMLAttributes, useState, useEffect } from 'react';
-import { IconBase } from 'react-icons';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
 
-import { IconBaseProps } from 'react-icons/lib';
 import { Container } from './styles';
+import { mainModule } from 'process';
+import { lookup } from 'dns';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
-  const [isFocused, setIsFocused] = useState(() => []);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect() => {
-    setIsFocused(!!isFocused);
-  }, [isFocused];
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
-  return
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
 }
 
 
