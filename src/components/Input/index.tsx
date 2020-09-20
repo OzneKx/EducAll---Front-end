b154e0c97ccfc3280e49c8f4d0099a507c8a5ca7
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-09-20 16:07:16 -0300

    EducAll - Front-end

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,30 +5,13 @@ import React, {
   useState,
   useCallback,
 } from 'react';
-import { IconBaseProps } from 'react-icons';
-import { FiAlertCircle } from 'react-icons/fi';
-import { useField } from '@unform/core';
 
-import { Container } from './styles';
-import { mainModule } from 'process';
-import { lookup } from 'dns';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon?: React.ComponentType<IconBaseProps>;
-}
-
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
-
-  const { fieldName, defaultValue, error, registerField } = useField(name);
-
-  const handleInputFocus = useCallback(() => {
-    setIsFocused(true);
-  }, []);
 }
 
 
